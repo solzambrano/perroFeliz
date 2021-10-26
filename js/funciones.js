d96a4eb3f9d0af538e769fd0c5b9f70e17d1399a
Soledad Zambrano <soe_28_16@hotmail.com>
2021-10-26 19:53:27 -0300

    avanceProyecto parte1

---
+++ js/funciones.js
@@ -0,0 +1,21 @@
+//para el navbar de la pagina
+$(function(){
+    $("#quien .cartas").hide()
+    $("#preguntas .cartas").hide()
+    $("#comentarios .cartas").hide()
+})
+$("#boton1").click(function(){
+    $("#quien .cartas").show()
+    $("#preguntas .cartas").hide()
+    $("#comentarios .cartas").hide()
+})
+$("#boton2").click(function(){
+    $("#preguntas .cartas").show()
+    $("#quien .cartas").hide()
+    $("#comentarios .cartas").hide()
+    })
+ $("#boton3").click(function(){
+        $("#comentarios .cartas").show()
+        $("#quien .cartas").hide()
+        $("#preguntas .cartas").hide()
+        })
